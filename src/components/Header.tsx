d1c498943c2bc38b6565465a2691740c06043336
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-04-30 16:47:02 +0300

    feat: separate in components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function  Header() {
+  return (
+    <header className="header">
+      <img src="/twitter_logo.png" alt="logo"/>
+    </header>
+  )
+
+}
+
+export default Header;
