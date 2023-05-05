3b72f5ca668c3612ff4217da59f7d283da9acf85
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-05 22:20:21 +0300

    refactor: divide into components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,56 +1,48 @@
 import React from "react";
 import style from "./Dialogs.module.scss"
 import {NavLink} from "react-router-dom";
+import {message} from "antd";
+
+type DialogPropsType = {
+  name: string
+  id: number
+}
+
+type MessagesPropsType = {
+  message: string
+}
+
+function DialogItem (props: DialogPropsType) {
+  return (
+    <div>
+      <NavLink
+        to={`/dialogs/${props.id}`}
+        className={style.people__item}
+        activeClassName={style.people__item_active}
+      >{props.name}
+      </NavLink>
+    </div>
+  )
+}
+
+function MessageItem (props: MessagesPropsType) {
+  return <div className={style.messages__item}>{props.message}</div>
+}
 
 function Dialogs() {
   return (
     <div className={style.dialogsContent}>
       <div className={`${style.people} ${style.dialogsContent__people}`}>
-        <div>
-          <NavLink
-            to="/dialogs/1"
-            className={style.people__item}
-            activeClassName={style.people__item_active}
-          >Alena
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/dialogs/2"
-            className={style.people__item}
-            activeClassName={style.people__item_active}
-          >Victor
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/dialogs/3"
-            className={style.people__item}
-            activeClassName={style.people__item_active}
-          >Jasmine
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/dialogs/4"
-            className={style.people__item}
-            activeClassName={style.people__item_active}
-          >Jon
-          </NavLink>
-        </div>
-        <div>
-          <NavLink
-            to="/dialogs/5"
-            className={style.people__item}
-            activeClassName={style.people__item_active}
-          >Max
-          </NavLink>
-        </div>
+        <DialogItem name={"Victor"} id={1} />
+        <DialogItem name={"Jon"} id={2} />
+        <DialogItem name={"Alex"} id={3} />
+        <DialogItem name={"Jasmine"} id={4} />
+        <DialogItem name={"Vladimir"} id={5} />
       </div>
       <div className={`${style.messages} ${style.dialogsContent__messages}`}>
-        <div className={style.messages__item}>Hello!</div>
-        <div className={style.messages__item}>How are you doing?</div>
-        <div className={style.messages__item}>I'm fine, thanks!</div>
+        <MessageItem message={"Hello!"} />
+        <MessageItem message={"How are you doing?"} />
+        <MessageItem message={"I'm fine, thanks!"} />
       </div>
     </div>
   )
