f634897ed8b1f96b6720054914ee968037fbc2b4
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-05-05 21:47:19 +0300

    feat: dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,57 @@
 import React from "react";
 import style from "./Dialogs.module.scss"
+import {NavLink} from "react-router-dom";
 
 function Dialogs() {
   return (
-    <div className={style.content}>
-      <h1>Hello, everybody</h1>
+    <div className={style.dialogsContent}>
+      <div className={`${style.people} ${style.dialogsContent__people}`}>
+        <div>
+          <NavLink
+            to="/dialogs/1"
+            className={style.people__item}
+            activeClassName={style.people__item_active}
+          >Alena
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/2"
+            className={style.people__item}
+            activeClassName={style.people__item_active}
+          >Victor
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/3"
+            className={style.people__item}
+            activeClassName={style.people__item_active}
+          >Jasmine
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/4"
+            className={style.people__item}
+            activeClassName={style.people__item_active}
+          >Jon
+          </NavLink>
+        </div>
+        <div>
+          <NavLink
+            to="/dialogs/5"
+            className={style.people__item}
+            activeClassName={style.people__item_active}
+          >Max
+          </NavLink>
+        </div>
+      </div>
+      <div className={`${style.messages} ${style.dialogsContent__messages}`}>
+        <div className={style.messages__item}>Hello!</div>
+        <div className={style.messages__item}>How are you doing?</div>
+        <div className={style.messages__item}>I'm fine, thanks!</div>
+      </div>
     </div>
   )
 }
