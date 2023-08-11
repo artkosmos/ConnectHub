ad65c90034b57768bc8db16f58e12902801517d3
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-11 15:46:39 +0300

    implement waiting while auth is accepted

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,39 @@ import UsersContainer from "./components/Users/UsersContainer";
 import RouterProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import {Login} from "./components/Login/Login";
+import {useSelector} from "react-redux";
+import {StateType} from "./redux/redux-store";
+import {Preloader} from "./components/Preloader/Preloader";
 
 function App() {
+
+  const isAuth = useSelector<StateType, boolean>(state => state.auth.isLogIn)
 
   return (
     <BrowserRouter>
       <div className="appWrapper">
         <HeaderContainer/>
         <div className="menuAndContentWrapper">
-          <Menu />
-          <div className="contentWrapper">
-            <Route exact path="/" render={() => <Redirect to={'/profile'}/>}/>
-            <Route exact path="/profile" component={RouterProfileContainer}/>
-            <Route exact path="/profile/:userId" component={RouterProfileContainer}/>
-            <Route path="/dialogs" component={DialogsContainer}/>
-            <Route path="/news" component={News}/>
-            <Route path="/music" component={Music}/>
-            <Route path="/users" component={UsersContainer} />
-            <Route path="/settings" component={Settings}/>
-            <Route path="/login" component={Login}/>
-          </div>
+          <Menu/>
+          {isAuth
+            ? <div className="contentWrapper">
+              <Route exact path="/" render={() => <Redirect to={'/profile'}/>}/>
+              <Route exact path="/profile" component={RouterProfileContainer}/>
+              <Route exact path="/profile/:userId" component={RouterProfileContainer}/>
+              <Route path="/dialogs" component={DialogsContainer}/>
+              <Route path="/news" component={News}/>
+              <Route path="/music" component={Music}/>
+              <Route path="/users" component={UsersContainer}/>
+              <Route path="/settings" component={Settings}/>
+              <Route path="/login" component={Login}/>
+            </div>
+            : <Preloader/>}
         </div>
       </div>
     </BrowserRouter>
   );
 }
+
 // Route(встроенный компонент) позволяет отслеживать изменения url, показывая необходимый компонент
 // NavLink(встроенный компонент) позволяет менять url страницы не перезагружая ее целиком
 export default App;
