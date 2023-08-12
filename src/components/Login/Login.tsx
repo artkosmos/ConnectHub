ebbad0a2a21c57a8c948fd4d35a03ea05a8706c3
Artem Kosmatkov <artemkosmatkov1994@gmail.com>
2023-08-12 17:59:41 +0300

    implement not connected login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React from "react"
+import { Form, Field } from 'react-final-form'
 
 export function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <LoginForm/>
     </div>
+  )
+}
+
+export function LoginForm() {
+  console.log('rerender')
+  return (
+    <Form
+      onSubmit={(data) => {
+        console.log(data)
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>First Name</label>
+            <Field name="firstName" component={'input'} placeholder="First Name" />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <Field name="lastName" component={'input'} placeholder="Last Name" />
+          </div>
+          <div>
+            <label>Remember me</label>
+            <Field name="rememberMe" type={'checkbox'} component={'input'} placeholder="Last Name" />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    />
   )
 }
